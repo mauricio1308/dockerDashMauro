52536709097139715c2b3a0daa1bd40e835d3148
Ubuntu <ubuntu@ip-172-31-84-134.ec2.internal>
2023-11-18 23:18:17 +0000

    Cambios en el .py

--- app/app.py
+++ app/app.py
@@ -13,7 +13,7 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 server = app.server
 
 # PREDICTION API URL 
-api_url = "http://44.211.196.239:8001/api/v1/predict"
+api_url = "http://54.234.18.139:8001/api/v1/predict"
 
 # Layout in HTML
 app.layout = html.Div(
